fe35e69f88744d1bc3a993d16fb0edb8412d1559
k01622944 <matthias.klampfer@yahoo.com>
2019-01-21 21:04:49 +0100

    database connection

--- src/generator/Main.java
+++ src/generator/Main.java
@@ -3,9 +3,22 @@ package generator;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
+        try{
+            Connection con=DriverManager.getConnection(
+                    "jdbc:mysql://db4free.net:3306/projektdke","gruppe1","Dke&Inheritance");
+            Statement stmt=con.createStatement();
+            ResultSet rs=stmt.executeQuery("select * from CBR_Performance");
+            System.out.println("ID \t | \t Parameter \t | \t Contexts");
+            while(rs.next())
+                System.out.println(rs.getInt(1)+"\t | \t "+rs.getString(2)+"\t\t\t | \t "+rs.getString(3));
+            con.close();
+        }catch(Exception e){ System.out.println(e);}
+
+
         String file = "cbr_output.txt";
 
         Properties p = new Properties();
